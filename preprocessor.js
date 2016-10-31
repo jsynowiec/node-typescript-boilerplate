44c941ae9da4bbf68f2a15f48b02494e31608bee
Jakub Synowiec <jakub.synowiec@gmail.com>
2016-10-31 19:42:05 +0100

    Load preprocessor ts options from tsconfig.json

--- preprocessor.js
+++ preprocessor.js
@@ -1,16 +1,16 @@
 // https://github.com/facebook/jest/blob/master/examples/typescript/preprocessor.js
 
 const tsc = require('typescript');
+const fs = require('fs');
+
+const compilerOptions = JSON.parse(fs.readFileSync('./tsconfig.json')).compilerOptions;
 
 module.exports = {
   process(src, path) {
     if (path.endsWith('.ts') || path.endsWith('.tsx')) {
       return tsc.transpile(
         src,
-        {
-          module: tsc.ModuleKind.CommonJS,
-          jsx: tsc.JsxEmit.React,
-        },
+        compilerOptions,
         path,
         []
       );
