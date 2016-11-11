c7b3126aab65b41158cacf7be0398169afdc91c2
Jakub Synowiec <github@jakubsynowiec.info>
2016-11-11 18:32:45 +0100

    Add more complex example

--- __tests__/main.spec.ts
+++ __tests__/main.spec.ts
@@ -1,5 +1,18 @@
-import { greeter } from '../src/main';
+import greeter from '../src/main';
 
-it('should greet a user', () => {
-  expect(greeter('John')).toBe('Hello, John');
+jest.useFakeTimers();
+
+let hello: string;
+
+beforeAll(async () => {
+  hello = await greeter('John');
+});
+
+it('delays the greeting by 2 seconds', () => {
+  expect((<jest.Mock<void>> setTimeout).mock.calls.length).toBe(1);
+  expect((<jest.Mock<void>> setTimeout).mock.calls[0][1]).toBe(2000);
+});
+
+it('greets a user with `Hello, {name}` message', () => {
+  expect(hello).toBe('Hello, John');
 });
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
-function greeter (name: string): string {
-    return `Hello, ${name}`;
+// Below is an example of using both, the tslint errors suppression
+
+function delayedHello(name: string, delay: number = 2000): Promise<string> {
+  // tslint:disable-next-line no-string-based-set-timeout
+  return new Promise((resolve) => setTimeout(resolve(`Hello, ${name}`), delay));
 }
 
-export {
-  greeter,
+export default async function greeter(name) { // tslint:disable-line
+  return await delayedHello(name);
 };
