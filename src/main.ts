8662daa2ec69fad826f8ec65386abfe7ba25a167
Jakub Synowiec <github@jakubsynowiec.info>
2017-01-04 09:54:46 +0100

    Add jsdoc example

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-// Below is an example of using both, the tslint errors suppression
+// Below is an example of using TSLint errors suppression
 
+/**
+ * Returns a Promise<string> that resolves after given time.
+ *
+ * @param {string} name - Somebody's name
+ * @param {number=} [delay=2000] - Number of milliseconds to delay resolution.
+ * @returns {Promise<string>}
+ */
 function delayedHello(name: string, delay: number = 2000): Promise<string> {
   // tslint:disable-next-line no-string-based-set-timeout
   return new Promise((resolve) => setTimeout(resolve(`Hello, ${name}`), delay));
