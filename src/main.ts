b1978e2f7353830bcec46e0a4e3c7c62a05acdd4
Francesco Soncina <phra@users.noreply.github.com>
2017-05-03 22:24:27 +0100

    fix setTimeout call, fixes #1

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@
  */
 function delayedHello(name: string, delay: number = 2000): Promise<string> {
   // tslint:disable-next-line no-string-based-set-timeout
-  return new Promise((resolve) => setTimeout(resolve(`Hello, ${name}`), delay));
+  return new Promise((resolve) => setTimeout(() => resolve(`Hello, ${name}`), delay));
 }
 
 export default async function greeter(name) { // tslint:disable-line
