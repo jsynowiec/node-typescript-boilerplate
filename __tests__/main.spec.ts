a7d2db7d7daf01d74638bae3cd24a6f70169541e
Jakub Synowiec <github@jakubsynowiec.info>
2017-05-04 14:10:44 +0200

    Deduplicate arrange step

--- __tests__/main.spec.ts
+++ __tests__/main.spec.ts
@@ -1,19 +1,23 @@
 import greeter from '../src/main';
 
-jest.useFakeTimers();
+describe('greeter function', () => {
+  jest.useFakeTimers();
 
-it('delays the greeting by 2 seconds', async () => {
-  expect.assertions(2);
-  const promise: Promise<string> = greeter('John');
-  jest.runOnlyPendingTimers();
-  await promise;
-  expect((<jest.Mock<void>> setTimeout).mock.calls.length).toBe(1);
-  expect((<jest.Mock<void>> setTimeout).mock.calls[0][1]).toBe(2000);
-});
+  let hello: string;
 
-it('greets a user with `Hello, {name}` message', async () => {
-  const promise: Promise<string> = greeter('John');
-  jest.runOnlyPendingTimers();
-  const hello: string = await promise;
-  expect(hello).toBe('Hello, John');
+  beforeAll(async () => {
+    const p: Promise<string> = greeter('John');
+    jest.runOnlyPendingTimers();
+    hello = await p;
+  });
+
+  it('delays the greeting by 2 seconds', () => {
+    expect((<jest.Mock<void>> setTimeout).mock.calls.length).toBe(1);
+    expect((<jest.Mock<void>> setTimeout).mock.calls[0][1]).toBe(2000);
+  });
+
+  it('greets a user with `Hello, {name}` message', () => {
+    expect(hello).toBe('Hello, John');
+  });
+
 });
