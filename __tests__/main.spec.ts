e9a667df4bafb1601d1a05527320c22db2250670
Jakub Synowiec <github@jakubsynowiec.info>
2017-09-10 11:42:54 +0200

    Refine example

--- __tests__/main.spec.ts
+++ __tests__/main.spec.ts
@@ -1,14 +1,16 @@
-import { greeter } from '../src/main';
+import { Delays, greeter } from '../src/main';
 
 describe('greeter function', () => {
   // Read more about fake timers: http://facebook.github.io/jest/docs/en/timer-mocks.html#content
   jest.useFakeTimers();
 
+  const name: string = 'John';
+
   let hello: string;
 
   // Act before assertions
   beforeAll(async () => {
-    const p: Promise<string> = greeter('John');
+    const p: Promise<string> = greeter(name);
     jest.runOnlyPendingTimers();
     hello = await p;
   });
@@ -16,12 +18,12 @@ describe('greeter function', () => {
   // Assert if setTimeout was called properly
   it('delays the greeting by 2 seconds', () => {
     expect((<jest.Mock<void>> setTimeout).mock.calls.length).toBe(1);
-    expect((<jest.Mock<void>> setTimeout).mock.calls[0][1]).toBe(2000);
+    expect((<jest.Mock<void>> setTimeout).mock.calls[0][1]).toBe(Delays.Long);
   });
 
   // Assert greeter result
   it('greets a user with `Hello, {name}` message', () => {
-    expect(hello).toBe('Hello, John');
+    expect(hello).toBe(`Hello, ${name}`);
   });
 
 });
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 /**
+ * Some predefined delays (in milliseconds).
+ */
+export enum Delays {
+  Short = 500,
+  Medium = 2000,
+  Long = 5000,
+}
+
+/**
  * Returns a Promise<string> that resolves after given time.
  *
- * @param {string} name - Somebody's name
- * @param {number=} [delay=2000] - Number of milliseconds to delay resolution.
+ * @param {string} name - A name.
+ * @param {number=} [delay=Delays.Medium] - Number of milliseconds to delay resolution of the Promise.
  * @returns {Promise<string>}
  */
-function delayedHello(name: string, delay: number = 2000): Promise<string> {
+function delayedHello(name: string, delay: number = Delays.Medium): Promise<string> {
   return new Promise<string>(
     (resolve: (value?: string | PromiseLike<string>) => void) => setTimeout(
       () => resolve(`Hello, ${name}`),
@@ -14,9 +23,10 @@ function delayedHello(name: string, delay: number = 2000): Promise<string> {
   );
 }
 
-// Below is an example of using TSLint errors suppression
-// Here it's supressing missing type definitions for greeter function
+// Below are examples of using TSLint errors suppression
+// Here it is supressing missing type definitions for greeter function
 
 export async function greeter(name) { // tslint:disable-line typedef
-  return await delayedHello(name); // tslint:disable-line no-unsafe-any
+  // tslint:disable-next-line no-unsafe-any
+  return await delayedHello(name, Delays.Long);
 }
